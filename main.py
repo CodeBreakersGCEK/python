6cdbf32c91a35e88bb4932b8743ec1651d046453
sahu-shashwat <shashwatsahu780@gmail.com>
2021-12-28 01:20:33 +0530

    init

---
+++ main.py
@@ -0,0 +1,58 @@
+# x=2*2*2
+# print(x)
+# y=2 ** 3
+# print(y)
+# a=10 / 3 # return intiger value # what is the diffrence between intiger and float
+# print(a)
+# b=10//3 # return whole number
+# print(b)
+# c=10%3 # return reminder
+# print(c)
+# d=(2+8)*3
+# print(d)
+
+
+x='Abhisek'
+print(x)
+y="Abhisek's Laptop" # to avoid this always use "" and vise versa
+print(y)
+
+x="India" * 10
+print(x)
+x="India\n" * 10
+print(x)
+x="c:\docs\navin"
+print(x)
+
+
+name="Abhise Samal"
+len(name)
+print(len(name))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[-1])
+# print(name[7]) # Learn how to read any error Index out of  range
+
+print(name + "Samal")
+
+c=9
+d=8
+print(c+d)
+# print(_+8)
+
+
+print("sorry "*100)
+set={1,2,2}
+print(set)
+list=[3,5,6,7]
+t=(3,5,6,7)
+t[0]="a"
+print(t)
+list[0]="z"
+print(list)
+# print(list)
---
+++ main2.py
@@ -0,0 +1,4 @@
+x="Abcd"
+print(x)
+y=9
+print(type(y))
