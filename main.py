7342db08a72c42030509a70b19c27e238a329dc2
Abhisek Samal <92312713+CodeAvk@users.noreply.github.com>
2022-01-16 11:58:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,148 @@
-# x=2*2*2
+# -------------------------------TUT-1- Variables in Python-----------------------------------------#
+
+# print("Python") # as the name suggested it's a function which is used to  print a python  objects .
+
+# Variables - Basically variable is a container where you can put your value
+# x=2 # Here x is the container and 2 is the value
 # print(x)
-# y=2 ** 3
-# print(y)
-# a=10 / 3 # return intiger value # what is the diffrence between intiger and float
-# print(a)
-# b=10//3 # return whole number
-# print(b)
-# c=10%3 # return reminder
-# print(c)
-# d=(2+8)*3
-# print(d)
 
-x = "Abhisek"
-print(x)
-y = "Abhisek's Laptop"  # to avoid this always use "" and vise versa
-print(y)
 
-x = "India" * 10
-print(x)
-x = "India\n" * 10
-print(x)
-x = "c:\docs\navin"
-print(x)
 
-name = "Abhise Samal"
-len(name)
+# Why it's called  variables ? -- Bcz we can change its value ..
+# intigers
+# x=3
+# print(x)
+
+# we dont need to decalre any type of variable
+
+
+
+# Strings - These are combinations of Character
+name="Python"
+# print(name)
+# print(name*2)
+
+# As we said that Strings are combination of Character,Now the question arise can we fetch the character ?
+# print(name[0])
+# print(name[3])
+# print(name[6]) # Show error string index out of range
+
+# we can also used negative numbers while fetching
+# when we use negative number its start from ending
+# name="Python"
+# print(name[-1])
+
+
+# fetching two character
+name="Python"
+# print(name[0:2])
+# print(name[1:4])
+# print(name[1:])
+# print(name[:4])
+# print(name[:3])
+
+
+name="Python"
+
+name[0]="C" # Throw Error
+print(name) # 'str' object does not support item assignment i.e string is immutable
+
+# counting Character in sting
+# name="Python"
 print(len(name))
-print(name[0])
-print(name[1])
-print(name[2])
-print(name[3])
-print(name[4])
-print(name[5])
-print(name[6])
-print(name[-1])
-# print(name[7]) # Learn how to read any error Index out of  range
 
-print(name + "Samal")
+#------------------------------------------------------Tut-2- SET and Operation on set-------------------------------------
 
-c = 9
-d = 8
-print(c + d)
-# print(_+8)
+# In Python List are the collection of data,maybe similar type or may not be
 
-
-print("sorry " * 100)
-set = {1, 2, 2}
-print(set)
-list = [3, 5, 6, 7]
-t = (3, 5, 6, 7)
-t[0] = "a"
-print(t)
-list[0] = "z"
+list=[1,2,3,4,5,6,7,8,9] # List in mutable
+list[0]=11 # Here "0" replace "1"
 print(list)
+# print(list[0])
+# print(list[1])
+# print(list[-1])
+
+list1=["C","C++","Java","Go","HTML","Python","Cython","Z"]
+# print(list1)
+
+
+
+
+list2=[99,78,"Java",90,"JavaScript"]
+# print(list2)
+
+
+# we can also make list of list
+
+# list3=[list,list1,list2]
+# print(list3)
+
+
+# Some Operations on list
+list=[1,2,3,4,5,6,7,8,9]
+
+
+# Append - In append the assignment was done at the end
+# list.append(10)
 # print(list)
+
+# insert
+# list=[1,2,3,4,5,6,7,8,9]
+# list.insert(2,77)
+# print(list)
+
+#extend- used to add multiple value
+# list=[1,2,3,4,5,6,7,8,9]
+# list.extend([7,77,777,90909,3,45])
+# print(list)
+
+
+
+
+
+
+# remove - remove by value
+# list=[1,2,3,4,5,6,7,8,9]
+# list.remove(7)
+# print(list)
+
+#pop-remove by index
+
+# list=[1,2,3,4,5,6,7,8,9]
+# list.pop(1)
+# print(list)
+
+
+
+# min-find minimum value
+
+# list2=[99,78,"Java",90,"JavaScript"]
+# a=min(list2) # Throw error
+# print(a)
+
+#min-find maximum value
+
+# list2=[99,78,"Java",90,"JavaScript"]
+# a=max(list2)
+# print(a)
+
+#sum
+# list=[1,2,3,4,5,6,7,8,9]
+# b=sum(list)
+# print(b)
+
+#sort
+list4=[1,2,3,4,5,6,7,8]
+
+a=list4.count(9)
+print(a)
+
+
+
+# list4.sort()
+#
+# print(list4)
+# list4.sort(reverse=True)
+# #
+# print(list4)
+
